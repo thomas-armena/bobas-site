259d68e16d52a85eba79d9a70c2bdd9ab9525330
thomas-armena <thomasarmena@gmail.com>
2021-07-04 12:47:01 -0400

    fix

--- bobas/components/Footer.js
+++ bobas/components/Footer.js
@@ -1,10 +1,11 @@
 import styles from '../styles/Footer.module.scss';
+import Link from 'next/link'
 
 function Footer() {
     return (
         <div className={styles.footer}>
-            <a href='/'> Home </a>
-            <a href='/privacyPolicy'> Privacy Policy </a>
+            <Link href='/'> Home </Link>
+            <Link href='/privacyPolicy'> Privacy Policy </Link>
         </div>
     )
 }
