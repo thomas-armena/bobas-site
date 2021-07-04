1a9b581c11404d4adae931682a4dd18080f937cc
thomas-armena <thomasarmena@gmail.com>
2021-07-04 17:05:55 -0400

    fix again

--- bobas/components/YoutubeEmbed.js
+++ bobas/components/YoutubeEmbed.js
@@ -3,7 +3,7 @@
 function YoutubeEmbed() {
     return (
         <div className="video-responsive">
-            <iframe width={375/2} height={812/2} src="https://www.youtube.com/embed/lTb7dQojpoY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>
+            <iframe width={375/2} height={812/2} src="https://www.youtube.com/embed/lTb7dQojpoY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     )
 }
